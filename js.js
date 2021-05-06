50d8923e6b5e7c371ea2d74ab6d14f7272983d3a
braco7 <44658162+braco7@users.noreply.github.com>
2021-05-05 23:25:21 -0300

    Se añade un objeto
    Se ha añadido una clase / objeto en el código para dar al usuario la posibilidad de escoger a cual producto se le da la cotización

--- js.js
+++ js.js
@@ -1,4 +1,50 @@
-let monto = prompt("Cual es el monto de su compra")
+class Producto {
+    constructor(id,precio,descripcion,stock) {
+        this.id=id;
+        this.precio=precio;
+        this.descripcion=descripcion;
+        this.stock=stock;
+    }
+    actualizaStock(){
+        this.stock = this.stock-1;
+    }
+}
+
+const producto1 = new Producto(1,2580,"Alimento 50kg",5);
+const producto2 = new Producto(2,350,"Juguete para perros hueso",4)
+const producto3 = new Producto (3,4900,"Casa para perros",2)
+const producto4 = new Producto(4,6900,"Casa para perro de lujo",6)
+
+// console.log(producto1)
+
+let productoId = prompt("Seleccione el producto (Elija un número) \n" + producto1.id + ".- " + producto1.descripcion + " ---> Precio: " + producto1.precio
++"\n" + producto2.id +  ".- "  + producto2.descripcion + " ---> Precio: " + producto2.precio
++"\n" + producto3.id +  ".- "  + producto3.descripcion + " ---> Precio: " + producto3.precio
++"\n" + producto4.id +  ".- "  + producto4.descripcion + " ---> Precio: " + producto4.precio)
+
+const buscarMonto = function(productoId) {
+    let monto = 0
+    switch(productoId) {
+        case "1":
+            monto = producto1.precio
+          break;
+        case "2":
+            monto = producto2.precio
+          break;
+        case "3":
+            monto = producto3.precio
+          break;
+          case "4":
+              monto = producto4.precio
+            break;
+        default:
+           alert = "Error"
+      }
+    return monto
+}
+
+
+let monto = buscarMonto(productoId)
 parseInt(monto)
 
 // Función de sumar (Utilizada a lo largo del programa), variable anónima
@@ -16,10 +62,10 @@ const descVeinte = (a) => a*0.80
 
 // Calcular el descuento a ser aplicado en base al monto de compra
 function calcularDescuento (d) {
-    if (d > 999 && d < 3001){
+    if (d > 999 && d < 5001){
         return descQuince(d)
     }
-    else if (d > 3000) {
+    else if (d > 5000) {
         return descVeinte(d)
     }
     else {
